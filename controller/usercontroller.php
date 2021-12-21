d371cce0bdc7f9946c9cf6294aebafff9a0acb3c
tuvinh0106 <tuvinh0106@gmail.com>
2021-12-21 23:28:43 +0700

    cap nhat code

--- controller/usercontroller.php
+++ controller/usercontroller.php
@@ -1,19 +1,4 @@
 <?php
-
-include_once '../utils/checkMail.php';
-include_once '../utils/checkPass.php';
-
-$email = $_POST['email'];
-$pass = $_POST['password'];
-
-if (checkEmail($email) && checkPass($pass)) {
-    session_start();
-    $_SESSION["email"] = $email;
-    header("Location:../view/index.php?Message=Wellcome to our website !!!");
-} else {
-    header("Location:../view/dangnhap.php?Message=Sign in failed!!! Please try again");
-}
-
 function checkEmailValid($email)
 {
     $pattern_email = "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/";
