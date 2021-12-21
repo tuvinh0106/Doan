ee7b7a4f57a406d45d57d3ca37f34878d99c61bb
tuvinh0106 <tuvinh0106@gmail.com>
2021-12-21 22:48:54 +0700

    them admin model

---
+++ admin/model/admin.php
@@ -0,0 +1,29 @@
+<?php
+
+class Admin
+{
+    private $name;
+    private $email;
+    private $password;
+
+    public function __construct($name, $mail, $pass)
+    {
+        $this->name = $name;
+        $this->email = $mail;
+        $this->password = $pass;
+    }
+
+    public function get_name()
+    {
+        return $this->name;
+    }
+    public function get_email()
+    {
+        return $this->email;
+    }
+    public function get_password()
+    {
+        return $this->password;
+    }
+}
+
