cd878cf93b602e015e5b883bc8a5cb3890b250cc
tuvinh0106 <tuvinh0106@gmail.com>
2021-12-21 23:05:18 +0700

    them sanpham va cap nhat user

--- model/user.php
+++ model/user.php
@@ -2,20 +2,30 @@
 
 class User
 {
-    private $name;
+    private $id;
+    private $user_name;
     private $email;
     private $password;
+    private $diachi;
+    private $sdt;
 
-    public function __construct($name, $mail, $pass)
+    public function __construct($id, $name, $email, $pass, $diachi, $sdt)
     {
-        $this->name = $name;
-        $this->email = $mail;
+        $this->id = $id;
+        $this->user_name = $name;
+        $this->email = $email;
         $this->password = $pass;
+        $this->diachi = $diachi;
+        $this->sdt = $sdt;
     }
-
-    public function get_name()
+    //get
+    public function get_id()
     {
-        return $this->name;
+        return $this->id;
+    }
+    public function get_username()
+    {
+        return $this->user_name;
     }
     public function get_email()
     {
@@ -24,5 +34,38 @@ class User
     public function get_password()
     {
         return $this->password;
+    }
+    public function get_diachi()
+    {
+        return $this->diachi;
+    }
+    public function get_sdt()
+    {
+        return $this->sdt;
+    }
+    //set
+    public function set_id($id)
+    {
+        $this->id = $id;
+    }
+    public function set_username($user_name)
+    {
+        $this->user_name = $user_name;
+    }
+    public function set_email($email)
+    {
+        $this->email = $email;
+    }
+    public function set_password($pass)
+    {
+        $this->password = $pass;
+    }
+    public function set_diachi($diachi)
+    {
+        $this->diachi = $diachi;
+    }
+    public function set_sdt($sdt)
+    {
+        $this->sdt = $sdt;
     }
 }
