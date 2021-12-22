5b885451ec9401dd928fba5585e6f0bb06a89097
tuvinh0106 <tuvinh0106@gmail.com>
2021-12-22 20:23:27 +0700

    them code xu ly san pham

--- controller/controllersanpham.php
+++ controller/controllersanpham.php
@@ -1,1 +1,23 @@
+<?php
+include_once '../model/sanpham.php';
 
+function get_listSanPham()
+{
+    $servername = "localhost";
+    $dbname = "web";
+    $username = "root";
+    $password = "";
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("Select * From sanpham");
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $listSP = array();
+    while ($row = $stmt->fetch()) {
+
+        array_push($listSP, new Sanpham($row['MaSP'], $row['TenSP'], $row['Size'], $row['SoLuong'], $row['GiaSP'], $row['Mota'], $row['HinhAnh']));
+    }
+    return  $listSP;
+}
+?>
