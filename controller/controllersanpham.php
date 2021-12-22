24bffc3e8289ffd61223e059b0bf6a253dd23aa1
tuvinh0106 <tuvinh0106@gmail.com>
2021-12-22 20:44:26 +0700

    them infor san pham

--- controller/controllersanpham.php
+++ controller/controllersanpham.php
@@ -38,5 +38,22 @@ function connectDatabase()
     }
     $conn = null;
 }
-
+function inforSanPham($masp)
+{
+    include_once '../model/sanpham.php';
+    $stmt = connectDatabase()->prepare("SELECT * FROM sanpham WHERE MaSP='$masp';");
+    $stmt->execute();
+    $sanpham = $stmt->fetchAll();
+    foreach ($sanpham as $row) {
+        $row[0];
+        $row[1];
+        $row[2];
+        $row[3];
+        $row[4];
+        $row[5];
+        $row[6];
+        $sanpham = new Sanpham($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6]);
+    }
+    return $sanpham;
+}
 ?>
