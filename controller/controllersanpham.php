6b33c79298898f9f5d8a6f4c5bb5c3ddedea0ced
TranDuy309 <duy34282@gmail.com>
2021-12-22 20:36:53 +0700

    them code connectData

--- controller/controllersanpham.php
+++ controller/controllersanpham.php
@@ -20,4 +20,23 @@ function get_listSanPham()
     }
     return  $listSP;
 }
+
+
+function connectDatabase()
+{
+    $servername = "localhost";
+    $database = "web";
+    $username = "root";
+    $password = "";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage;
+        die();
+    }
+    $conn = null;
+}
+
 ?>
